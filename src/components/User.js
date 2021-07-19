647fb4dfee9c6010dcabfd7569a368a391b2048a
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-07-19 08:56:11 +0100

    #2 - Fetching Data

--- src/components/User.js
+++ src/components/User.js
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const User = () => {
+    const [profile, setProfile] = useState(null)
+    
+    useEffect(() => { //runs after the component has been rendered
+        setTimeout( async () => {
+            const res = await fetch('https://jsonplaceholder.typicode.com/users/1');
+            const data = await res.json();
+            setProfile(data);
+        }, 5000)
+    })
+
     return (
         <div className="user">
             <h2>User Details</h2>
+
+            {profile && (
+            <div className="profile">
+                <h3>{profile.username}</h3>
+                <p>{profile.email}</p>
+                <a href={profile.website}>{profile.website}</a>
+            </div>
+        )}
+
+        {!profile && <div>Loading...</div>}
         </div>
+
+        
     )
 }
 
